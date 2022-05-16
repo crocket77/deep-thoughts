bb1edb2cb17155fa9dd47f4c69e66b769c84ef9b
crocket77 <ethanpfeiffer345@outlook.com>
2022-05-16 16:39:56 -0500

    added functionality for adding a reaction and thought

--- client/src/App.js
+++ client/src/App.js
@@ -58,7 +58,11 @@ function App() {
                 element={<Signup />} 
               />
               <Route 
-                path="/profile" 
+                path="/profile/:username" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="/profile/" 
                 element={<Profile />} 
               />
               <Route 
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
 import ReactionList from '../components/ReactionList';
-
 import { useQuery } from '@apollo/client';
+import ReactionForm from '../components/ReactionForm';
+import Auth from '../utils/auth';
 import { QUERY_THOUGHT } from '../utils/queries';
 
 const SingleThought = (props) => {
@@ -36,6 +36,7 @@ const SingleThought = (props) => {
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions} />
       )}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
